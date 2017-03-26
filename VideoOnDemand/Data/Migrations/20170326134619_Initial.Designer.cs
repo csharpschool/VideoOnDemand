b7381f821438229c2d46d57b4705e23976906aad
Jonas <N/A>
2017-03-26 16:12:49 +0200

    Changed the connection string, added a migration and created the database

--- VideoOnDemand/Data/Migrations/20170326134619_Initial.Designer.cs
+++ VideoOnDemand/Data/Migrations/20170326134619_Initial.Designer.cs
@@ -8,9 +8,10 @@ using VideoOnDemand.Data;
 namespace VideoOnDemand.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170326134619_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
