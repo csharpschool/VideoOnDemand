ec6a0d66b1a1f3c44970f2db853951617064400b
Jonas <N/A>
2017-03-26 17:03:05 +0200

    Added the SignInManager to the HomeController and added the login.css stylesheet.

--- VideoOnDemand/Controllers/HomeController.cs
+++ VideoOnDemand/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using VideoOnDemand.Models;
 
 namespace VideoOnDemand.Controllers
 {
     public class HomeController : Controller
     {
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public HomeController(SignInManager<ApplicationUser> signInMgr)
+        {
+            _signInManager = signInMgr;
+        }
+
         public IActionResult Index()
         {
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToAction("Login", "Account");
+
             return View();
         }
 
