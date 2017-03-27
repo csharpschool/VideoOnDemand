221b619cfcfd3d85a0230c8f1cc6ac12f66c6f17
Jonas <N/A>
2017-03-27 18:20:59 +0200

    Added DTOs and View Models

---
+++ VideoOnDemand/Models/DTOModels/LessonInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace VideoOnDemand.Models.DTOModels
+{
+    public class LessonInfoDTO
+    {
+        public int LessonNumber { get; set; }   // ex: Lesson 1/5
+        public int NumberOfLessons { get; set; }
+        public int PreviousVideoId { get; set; }
+        public int NextVideoId { get; set; }
+        public string NextVideoTitle { get; set; }
+        public string NextVideoThumbnail { get; set; }
+    }
+}
