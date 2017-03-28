065a1a1691022fded167c5a24b76cfee46845f1b
Jonas <N/A>
2017-03-28 10:24:19 +0200

    Added Entity classes

---
+++ VideoOnDemand/Entities/Download.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoOnDemand.Entities
+{
+    public class Download
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(80), Required]
+        public string Title { get; set; }
+        [MaxLength(1024)]
+        public string Url { get; set; }
+
+        public int ModuleId { get; set; }
+        public int CourseId { get; set; } // Side-step from 3rd normal form for easier access to a video's course
+    }
+}
---
+++ VideoOnDemand/Entities/UserCourse.cs
@@ -0,0 +1,9 @@
+﻿namespace VideoOnDemand.Entities
+{
+    public class UserCourse
+    {
+        public string UserId { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
