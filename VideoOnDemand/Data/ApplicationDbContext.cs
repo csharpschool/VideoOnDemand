c24f2ce76b634a8d93025f5466c938e4d2266b0b
Jonas <N/A>
2017-04-30 10:01:03 +0200

    Added the entity classes to the ApplicationDbContext class.

--- VideoOnDemand/Data/ApplicationDbContext.cs
+++ VideoOnDemand/Data/ApplicationDbContext.cs
@@ -5,11 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using VideoOnDemand.Models;
+using VideoOnDemand.Entities;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace VideoOnDemand.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Download> Downloads { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<UserCourse> UserCourses { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
@@ -21,6 +30,15 @@ namespace VideoOnDemand.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            // Composite key
+            builder.Entity<UserCourse>().HasKey(uc => new { uc.UserId, uc.CourseId });
+
+            // Restrict cascading deletes
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
         }
     }
 }
