a3de024dc8ff632ea3b23c37c052a236e03f773d
Jonas <N/A>
2017-05-02 12:46:27 +0200

    Added the Details action and view for the UserCourses controller.

--- VideoOnDemand/Controllers/UserCoursesController.cs
+++ VideoOnDemand/Controllers/UserCoursesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 using VideoOnDemand.Data;
 using VideoOnDemand.Models;
 using VideoOnDemand.Models.DTOModels;
@@ -23,6 +25,26 @@ namespace VideoOnDemand.Controllers
             var model = _db.Courses
                 .Join(_db.UserCourses, c => c.Id, uc => uc.CourseId, (c, uc) => new { Courses = c, UserCourses = uc })
                 .Select(s => new UserCourseDTO { CourseId = s.Courses.Id, CourseTitle = s.Courses.Title, UserId = s.UserCourses.UserId, UserEmail = _userStore.Users.FirstOrDefault(u => u.Id.Equals(s.UserCourses.UserId)).Email });
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(string userId, int courseId)
+        {
+            if (userId == null || courseId.Equals(default(int)))
+            {
+                return NotFound();
+            }
+
+            var model = await _db.Courses
+                .Join(_db.UserCourses, c => c.Id, uc => uc.CourseId, (c, uc) => new { Courses = c, UserCourses = uc })
+                .Select(s => new UserCourseDTO { CourseId = s.Courses.Id, CourseTitle = s.Courses.Title, UserId = s.UserCourses.UserId, UserEmail = _userStore.Users.FirstOrDefault(u => u.Id.Equals(s.UserCourses.UserId)).Email })
+                .FirstOrDefaultAsync(w => w.CourseId.Equals(courseId) && w.UserId.Equals(userId));
+
+            if (model == null)
+            {
+                return NotFound();
+            }
 
             return View(model);
         }
