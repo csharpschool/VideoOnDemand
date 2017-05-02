b79435231ca1627afc69a5ba53a1c45c0e9fc73a
Jonas <N/A>
2017-05-02 11:03:27 +0200

    Added the Index action and view.

--- VideoOnDemand/Controllers/UserCoursesController.cs
+++ VideoOnDemand/Controllers/UserCoursesController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using VideoOnDemand.Data;
 using VideoOnDemand.Models;
+using VideoOnDemand.Models.DTOModels;
 
 namespace VideoOnDemand.Controllers
 {
@@ -14,6 +16,15 @@ namespace VideoOnDemand.Controllers
         {
             _db = userStore.Context;
             _userStore = userStore;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Courses
+                .Join(_db.UserCourses, c => c.Id, uc => uc.CourseId, (c, uc) => new { Courses = c, UserCourses = uc })
+                .Select(s => new UserCourseDTO { CourseId = s.Courses.Id, CourseTitle = s.Courses.Title, UserId = s.UserCourses.UserId, UserEmail = _userStore.Users.FirstOrDefault(u => u.Id.Equals(s.UserCourses.UserId)).Email });
+
+            return View(model);
         }
     }
 }
