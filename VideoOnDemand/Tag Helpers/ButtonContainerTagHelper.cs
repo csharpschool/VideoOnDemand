41887ccc81772af9ada3ca22957ce326fdc3580f
Jonas <N/A>
2017-05-03 09:26:57 +0200

    Added the button-container Tag Helper.

---
+++ VideoOnDemand/Tag Helpers/ButtonContainerTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VideoOnDemand.Tag_Helpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("button-container")]
+    public class ButtonContainerTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+        }
+    }
+}
