180110518e277c656711213dc9206a6370a5cb68
Jonas <N/A>
2017-05-03 13:30:17 +0200

    Added the 1st version of the button-container Tag Helper.

--- VideoOnDemand/Tag Helpers/ButtonContainerTagHelper.cs
+++ VideoOnDemand/Tag Helpers/ButtonContainerTagHelper.cs
@@ -11,12 +11,34 @@ namespace VideoOnDemand.Tag_Helpers
     [HtmlTargetElement("button-container")]
     public class ButtonContainerTagHelper : TagHelper
     {
+        #region Properties
+        public string Controller { get; set; }
+        public string Actions { get; set; }
+        #endregion
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (context == null) throw new ArgumentNullException(nameof(context));
             if (output == null) throw new ArgumentNullException(nameof(output));
 
             base.Process(context, output);
+
+            output.TagName = "span";    // Replaces <button-container> with <span> tag
+            output.Content.SetHtmlContent("<span style='min-width:100px; display:inline-block;'>");
+
+            var href = "";
+            var action = Actions;
+
+            if (Controller != null && Controller.Length > 0)
+            {
+                if (action != null && action.Length > 0)
+                    href = $@"href='/admin/{Controller}/{action}'";
+                else href = $@"href='/admin/{Controller}/Index'";
+            }
+
+            output.Content.AppendHtml($@"<a {href}>My link</a>");
+
+            output.Content.AppendHtml("</span>");
         }
     }
 }
